b741e5ef4dee32d04af77b74f4ed1af7d0544939
Kyialll <119960477+Kyialll@users.noreply.github.com>
2023-02-17 22:26:15 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Button from "./components/Button";
+import Text from "./components/Text";
+import Info from "./components/Info";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+        <Text/>
+      <Button/>
+        <Info title={'Thank you'}/>
+        <Info title={'for your attention.'}/>
+        <Info title={'Goodbye!'}/>
+        <Footer/>
+
+
     </div>
   );
 }
